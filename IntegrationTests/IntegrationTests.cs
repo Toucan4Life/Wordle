393bac176b41ca0bf72837fb39042c12e486413a
Toucan4Life <tdp94@hotmail.com>
2022-02-12 16:34:53 +0100

    enhanced main loop

--- IntegrationTests/IntegrationTests.cs
+++ IntegrationTests/IntegrationTests.cs
@@ -31,61 +31,24 @@ namespace IntegrationTests
             var result = _solver.CalculateEntropy("feuille", possibleSolution);
             Assert.IsTrue(Math.Abs(result - 6.908886) < 0.000001);
         }
-        [TestMethod]
-        public void CalculateEntropy()
-        {
-            var word = "usagers";
-            var wordDico = new CsvReader().GetAllWords("Lexique381.csv").Where(t => t.Key.Length == 7)
-                .ToDictionary(t => t.Key, t => t.Value);
 
-            var _solver = new WordleSolver();
+        //private bool IsAllowedPattern(string word, IEnumerable<Pattern> pattern)
+        //{
+        //    //RIER
+        //    //MXXI Allowed
+        //    //IXXM Not Allowed
+        //    //=> For the same letters, the I pattern should always be last
+        //    var test = pattern.Select((pat, i) => new {character = word[i], index = i, pat}).GroupBy(t => t.character);
 
-            var allPossiblePatternCombination = CartesianProduct(word.Select(_ => new List<Pattern>
-                {Pattern.Correct, Pattern.Incorrect, Pattern.Misplaced})).Where(pat=>IsAllowedPattern(word,pat));
+        //    bool any = test
+        //        .Where(tuple =>
+        //            tuple.Any(t => t.pat == Pattern.Misplaced) &&
+        //            tuple.Any(t => t.pat == Pattern.Incorrect)).Any(tuple =>
+        //            tuple.Where(t => t.pat == Pattern.Misplaced).Max(t => t.index) >
+        //            tuple.Where(t => t.pat == Pattern.Incorrect).Min(t => t.index));
 
-            var testDictionarry = new Dictionary<string, float>();
-            float totalWord = wordDico.Count(t => t.Key.Length==word.Length);
-
-            var enumerable = allPossiblePatternCombination.Select(pattern => {
-                var wordSearcher = new WordSearcher(wordDico) { WordLength = word.Length };
-                
-                var keyValuePairs = _solver.Filter(word, pattern, wordSearcher);
-                keyValuePairs.ForEach(t => testDictionarry.Add(t.Key, t.Value));
-                return keyValuePairs.Count / totalWord;
-            }).OrderByDescending(t=>t);
-
-            Assert.IsTrue(Math.Abs(enumerable.Sum() - 1f) < 0.0000001);
-        }
-
-        private bool IsAllowedPattern(string word, IEnumerable<Pattern> pattern)
-        {
-            //RIER
-            //MXXI Allowed
-            //IXXM Not Allowed
-            //=> For the same letters, the I pattern should always be last
-            var test = pattern.Select((pat, i) => new {character = word[i], index = i, pat}).GroupBy(t => t.character);
-
-            bool any = test
-                .Where(tuple =>
-                    tuple.Any(t => t.pat == Pattern.Misplaced) &&
-                    tuple.Any(t => t.pat == Pattern.Incorrect)).Any(tuple =>
-                    tuple.Where(t => t.pat == Pattern.Misplaced).Max(t => t.index) >
-                    tuple.Where(t => t.pat == Pattern.Incorrect).Min(t => t.index));
-
-            return !any;
-        }
-
-        public IEnumerable<IEnumerable<T>> CartesianProduct<T>(
-            IEnumerable<IEnumerable<T>> sequences)
-        {
-            IEnumerable<IEnumerable<T>> emptyProduct = new[] { Enumerable.Empty<T>() };
-            return sequences.Aggregate(
-                emptyProduct,
-                (accumulator, sequence) =>
-                    from accseq in accumulator
-                    from item in sequence
-                    select accseq.Concat(new[] { item }));
-        }
+        //    return !any;
+        //}
 
     }
 }
